a98401a733475bfe47d4533d4e7e5fe007f94397
SHUBH9130 <shubhamsone007@gmail.com>
2023-11-24 15:33:28 +0530

    First Commit

---
+++ Test.java
@@ -0,0 +1,15 @@
+package com.ss;
+
+import java.util.Date;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("-----------------------------");
+		System.out.println("Hello from java test ");
+		System.out.println("Date::" +new Date());
+		System.out.println("-----------------------------");
+	}
+
+}
